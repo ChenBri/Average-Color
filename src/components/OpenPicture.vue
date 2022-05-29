10b0f2b0a824a8488b144c5d2759c0409f097116
Chen Brilling <chenbrilling@gmail.com>
2022-05-29 19:44:43 +0300

    Added picture preview

---
+++ src/components/OpenPicture.vue
@@ -0,0 +1,76 @@
+<template>
+  <section>
+    <p class="text-xl pb-2">Import your image below:</p>
+
+    <input
+      type="file"
+      id="inpFile"
+      name="img"
+      accept="image/*"
+      @change="getFile"
+    />
+
+    <div id="image-preview-container">
+      <img src="" alt="Image Preview" id="image-preview-image" />
+
+      <span id="image-preview-text">Image Preview</span>
+    </div>
+  </section>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  methods: {
+    getFile() {
+      const inpFile = document.getElementById("inpFile");
+      const previewImage = this.$el.querySelector("#image-preview-image");
+      const previewText = this.$el.querySelector("#image-preview-text");
+
+      const file = inpFile.files[0];
+
+      if (file) {
+        const reader = new FileReader();
+
+        previewText.style.display = "none";
+        previewImage.style.display = "block";
+
+        reader.addEventListener("load", function () {
+          previewImage.setAttribute("src", this.result);
+          console.log(this);
+        });
+
+        reader.readAsDataURL(file);
+      } else {
+        previewText.style.display = null;
+        previewImage.style.display = null;
+        previewImage.setAttribute("src", "")
+      }
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+#image-preview-container {
+  width: 300px;
+  min-height: 100px;
+  border: 2px solid lightgray;
+  margin-top: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+  color: gray;
+}
+
+#image-preview-image {
+  display: none;
+  width: 100%;
+}
+</style>
---
+++ src/components/PageContainer.vue
@@ -0,0 +1,18 @@
+<template>
+<section>
+  <div class="border-2 border-black bg-slate-100 w-6/12 m-auto p-6 mt-24">
+   <header class="text-center text-3xl pb-8">Average Color</header>
+    <open-picture></open-picture>
+  </div>
+  </section>
+</template>
+
+<script>
+import OpenPicture from './OpenPicture.vue'
+
+export default {
+  components: {
+    OpenPicture
+  }
+}
+</script>
