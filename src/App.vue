831ed2875226fd205623e6b6e69c042dea4714b0
Chen Brilling <chenbrilling@gmail.com>
2022-06-03 18:49:09 +0300

    Changed project name and title

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <page-container> </page-container>
+    <page-container> </page-container>
 </template>
 
 <script>
 import PageContainer from "./components/PageContainer.vue";
 
 export default {
-  name: "App",
-  components: {
-    PageContainer,
-  },
+    name: "App",
+    components: {
+        PageContainer,
+    },
+    mounted() {
+        document.title = "Color Average";
+    },
 };
 </script>
